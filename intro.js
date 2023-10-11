6f693a15b85454ee3bce9f909ffd8499ae2c5c76
Mayurimk98 <mayurimkatwe@gmail.com>
2023-10-11 09:03:48 +0530

    first commit

---
+++ intro.js
@@ -0,0 +1,44 @@
+//javascript ==> .js
+
+//printing statement
+console.log("Mayuri")
+
+console.log(10)
+
+console.log(20 + 70) //20+70
+
+//datatype 
+//name, char sentence, text ==> ""  => string
+//10,20, 0.1, -090 => number
+
+
+//information store
+//let
+//const
+
+let firstName = "mayuri"
+//keyword variableName=value
+
+console.log(firstName)
+console.log(firstName)
+console.log(firstName)
+console.log(firstName)
+
+//update ==> sham
+//variableName=newvalue
+
+firstName = "sham" //update
+console.log(firstName)
+
+//const
+const marks = 30
+console.log(marks)
+
+//const cant perform update operation
+marks = 50
+console.log(marks)
+
+//comments  ==> user undestanding
+
+
+//1000
