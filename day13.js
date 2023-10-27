5604272a78c6510c0e120d1f533a2a489cc8f7ac
Mayurimk98 <mayurimkatwe@gmail.com>
2023-10-27 10:08:26 +0530

    27 oct

---
+++ day13.js
@@ -0,0 +1,100 @@
+
+
+// let fruit="apple"
+// //ppl
+
+// console.log(fruit.slice(1,4))
+
+
+// let flower="Jasmin chameli"
+// //jasmin => string
+
+// let y1=flower.split(" ")
+// console.log(y1) //["jasmin","chameli"]
+
+// console.log(y1[0])
+
+// let name1="ram sham sita druve"
+// let y2=name1.split(" ")
+// console.log(y2) //["ram","sham","sita","druve"]
+
+// //"ram-sham-sita-druve"
+// let y3=y2.join("-")
+// console.log(y3)//string
+
+
+
+
+// let a=[1,2,3]
+// let b=[4,5,6]
+// //two array join => concat
+// //array element ==> join
+
+// //CharAt()
+
+// // let firstName="vaibhav"
+
+// // //v   a   i   b   h   a   v
+// // //0   1   2   3  4    5  6
+
+// // let u1=firstName.charAt(1)
+// // console.log(u1)
+
+// // let u2=firstName.charAt(5)
+// // console.log(u2)
+
+// // let u3=firstName.charAt(10)
+// // console.log(u3) //"" => blank string
+
+// //CharCodeAT()
+
+// let firstName="vaibhav"
+
+// //v   a   i   b   h   a   v
+// //0   1   2   3  4    5  6
+
+// let p1=firstName.charCodeAt(4)
+// console.log(p1)
+
+// let p2=firstName.charCodeAt(9)
+// console.log(p2) //NaN ==> not a number
+
+
+// //method chaining
+
+// let fruits="banana"
+// fruits.toUpperCase().toLowerCase().indexOf('n')
+// //BANANA
+// //banana
+// //2 ==> number
+
+let firstName="Mayuri"
+let lastName="Katwe"
+let skill="javascript"
+
+//my firstName is Mayuri and my lastName is Katwe and I am learning Javascript
+
+console.log("my firstName is" +firstName+"and my lastName is"+lastName+"and i am learing"+skill )
+//my firstName isMayuriand my lastName isKatweand i am learingjavascript
+console.log("my firstName is " +firstName+" and my lastName is "+lastName+" and i am learing "+skill )
+//my firstName is Mayuri and my lastName is Katwe and i am learing javascript
+//+
+
+//string interpolation
+
+console.log(`My firstName is ${firstName} and my lastName is ${lastName} and i am learing ${skill}`)
+
+let a=20
+let b=30
+
+console.log(a+b) //number
+console.log(`${a+b}`)//string
+
+
+
+
+
+
+
+
+
