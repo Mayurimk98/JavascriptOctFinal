0db1985b2f2449596a36564edfe5ae0552057c12
Mayurimk98 <mayurimkatwe@gmail.com>
2023-11-08 09:07:54 +0530

    8 11

---
+++ day18.js
@@ -0,0 +1,38 @@
+
+// let obj={
+//     fn:"mayuri",
+//     ln:"katwe",
+//     display:function(){
+//         console.log("welcome all")
+//     }
+// }
+
+// //method => function
+
+// console.log(obj.fn)//mayuri
+
+// console.log(obj.display)//function
+
+// obj.display()//welcome all
+
+
+
+// let car={
+//     wheel:4
+// }
+
+// console.log(car.wheel)//4
+
+
+//object => key value => property
+//methods
+
+//string 
+//property=> length
+//methods=> toUpperCase()
+
+
+//DOM==> document object module
+//html
+//css
+//js
